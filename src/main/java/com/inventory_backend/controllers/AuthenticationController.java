b4ad1f5989faad25af163826f79cc1ce709340eb
elmehdi rezoug <elmehdirezoug100@gmail.com>
2025-04-03 17:37:21 +0000

    authentication not working commit

---
+++ src/main/java/com/inventory_backend/controllers/AuthenticationController.java
@@ -0,0 +1,34 @@
+package com.inventory_backend.controllers;
+
+import com.inventory_backend.dto.AuthenticationRequest;
+import com.inventory_backend.dto.AuthenticationResponse;
+import com.inventory_backend.dto.RegisterRequest;
+import com.inventory_backend.services.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationService service;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody RegisterRequest request
+    ) {
+        return ResponseEntity.ok(service.register(request));
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(
+            @RequestBody AuthenticationRequest request
+    ) {
+        return ResponseEntity.ok(service.authenticate(request));
+    }
+}
---
+++ src/main/java/com/inventory_backend/entities/Role.java
@@ -0,0 +1,6 @@
+package com.inventory_backend.entities;
+
+public enum Role {
+    USER,
+    ADMIN
+}
